238fbbf04d7e26f47883f13028fb8cd779c06f67
Andrew Walker <awalker125@users.noreply.github.com>
2016-02-29 07:52:33 +0000

    moved to pure jquery

--- src/main/webapp/js/execercise-app.js
+++ src/main/webapp/js/execercise-app.js
@@ -1,96 +1,188 @@
-
-$(document).ajaxSend(function(e, xhr, options) 
-{
-    xhr.setRequestHeader("username", "awalker");
-    xhr.setRequestHeader("api_key", "ueh32ueb23be32e73e723e2e7h2e7h23eibhvhv");
+$(document).ajaxSend(function(e, xhr, options) {
+	xhr.setRequestHeader("username", "awalker");
+	xhr.setRequestHeader("api_key", "ueh32ueb23be32e73e723e2e7h2e7h23eibhvhv");
 });
 
+// The root URL for the RESTful services
+var rootURL = "https://localhost:10443/training/webapi/excercise";
 
-var ExcerciseModel = Backbone.Model.extend({
-	idAttribute : 'name',
-	defaults : {
-		name : null,
-		tags : null
+var currentExcercise;
+
+// Retrieve excercise list when application starts
+findAll();
+
+// Nothing to delete in initial application state
+$('#btnDelete').hide();
+
+// Register listeners
+$('#btnSearch').click(function() {
+	search($('#searchKey').val());
+	return false;
+});
+
+// Trigger search when pressing 'Return' on search key input field
+$('#searchKey').keypress(function(e) {
+	if (e.which == 13) {
+		search($('#searchKey').val());
+		e.preventDefault();
+		return false;
 	}
 });
 
-var ExcerciseCollection = Backbone.Collection.extend({
-	url : 'https://localhost:10443/training/webapi/excercise/',
-	model : ExcerciseModel
+$('#btnAdd').click(function() {
+	newExcercise();
+	return false;
 });
 
-// var excercises = new ExcerciseCollection();
-
-// excercises.fetch().then(function() {
-// console.log(excercises.length); // >> length: 1
-// });
-
-// View class for displaying each excercise list item
-var ExcercisesListItemView = Backbone.View.extend({
-	tagName : 'li',
-	className : 'excercise',
-	template : _.template($('#excercise-item-tmpl').html()),
-
-	initialize : function() {
-		this.listenTo(this.model, 'destroy', this.remove)
-	},
-
-	render : function() {
-		var html = this.template(this.model.toJSON());
-		this.$el.html(html);
-		return this;
-	},
-
-	events : {
-		'click .remove' : 'onRemove'
-	},
-
-	onRemove : function() {
-		this.model.destroy();
-	}
+$('#btnSave').click(function() {
+	if ($('#excerciseId').val() == '')
+		addExcercise();
+	else
+		updateExcercise();
+	return false;
 });
 
-// View class for rendering the list of all excercises
-var ExcercisesListView = Backbone.View.extend({
-	el : '#excercises-app',
+$('#btnDelete').click(function() {
+	deleteExcericse();
+	return false;
+});
 
-	initialize : function() {
-		this.listenTo(this.collection, 'sync', this.render);
-	},
+//$('#excerciseList a').live('click', function() {
+//	findById($(this).data('identity'));
+//});
 
-	render : function() {
-		var $list = this.$('ul.excercises-list').empty();
+// Replace broken images with generic excercise bottle
+$("img").error(function() {
+	$(this).attr("src", "pics/generic.jpg");
 
-		this.collection.each(function(model) {
-			var item = new ExcercisesListItemView({
-				model : model
-			});
-			$list.append(item.render().$el);
-		}, this);
+});
 
-		return this;
-	},
+function search(searchKey) {
+	if (searchKey == '')
+		findAll();
+	else
+		findByName(searchKey);
+}
 
-	events : {
-		'click .create' : 'onCreate'
-	},
+function newExcercise() {
+	$('#btnDelete').hide();
+	currentExcercise = {};
+	renderDetails(currentExcercise); // Display empty form
+}
 
-	onCreate : function() {
-		var $name = this.$('#excercise-name');
+function findAll() {
+	console.log('findAll');
+	$.ajax({
+		type : 'GET',
+		url : rootURL,
+		dataType : "json", // data type of response
+		success : renderList
+	});
+}
 
-		if ($name.val()) {
-			this.collection.create({
-				name : $name.val()
-			});
+function findByName(searchKey) {
+	console.log('findByName: ' + searchKey);
+	$.ajax({
+		type : 'GET',
+		url : rootURL + '/search/name/' + searchKey,
+		dataType : "json",
+		success : renderList
+	});
+}
 
-			$name.val('');
+function findById(id) {
+	console.log('findById: ' + id);
+	$.ajax({
+		type : 'GET',
+		url : rootURL + '/' + id,
+		dataType : "json",
+		success : function(data) {
+			$('#btnDelete').show();
+			console.log('findById success: ' + data.name);
+			currentExcercise = data;
+			renderDetails(currentExcercise);
 		}
-	}
-});
+	});
+}
 
-// Create a new list collection, a list view, and then fetch list data:
-var excerciseList = new ExcerciseCollection();
-var excerciseView = new ExcercisesListView({
-	collection : excerciseList
-});
-excerciseList.fetch();
+function addExcercise() {
+	console.log('addExcercise');
+	$.ajax({
+		type : 'POST',
+		contentType : 'application/json',
+		url : rootURL,
+		dataType : "json",
+		data : formToJSON(),
+		success : function(data, textStatus, jqXHR) {
+			alert('Wine created successfully');
+			$('#btnDelete').show();
+			$('#excerciseId').val(data.id);
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			alert('addExcercise error: ' + textStatus);
+		}
+	});
+}
+
+function updateExcercise() {
+	console.log('updateExcercise');
+	$.ajax({
+		type : 'PUT',
+		contentType : 'application/json',
+		url : rootURL + '/' + $('#excerciseId').val(),
+		dataType : "json",
+		data : formToJSON(),
+		success : function(data, textStatus, jqXHR) {
+			alert('Excercise updated successfully');
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			alert('updateExcercise error: ' + textStatus);
+		}
+	});
+}
+
+function deleteExcericse() {
+	console.log('deleteExcercise');
+	$.ajax({
+		type : 'DELETE',
+		url : rootURL + '/' + $('#excerciseId').val(),
+		success : function(data, textStatus, jqXHR) {
+			alert('Excercise deleted successfully');
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			alert('deleteExcercise error');
+		}
+	});
+}
+
+function renderList(data) {
+	// JAX-RS serializes an empty list as null, and a 'collection of one' as an
+	// object (not an 'array of one')
+	var list = data == null ? [] : (data instanceof Array ? data : [ data ]);
+
+	$('#excerciseList li').remove();
+	$.each(list, function(index, excerise) {
+		
+		$('#excerciseList').append(
+				
+				'<li>' + excerise.name + '</li>'
+				//'<li><a href="#" data-identity="' + excerise.id + '">'
+				//		+ excerise.name + '</a></li>'
+				
+		);
+	});
+}
+
+function renderDetails(excercise) {
+	$('#excerciseId').val(excercise.id);
+	$('#name').val(excercise.name);
+}
+
+// Helper function to serialize all the form fields into a JSON string
+function formToJSON() {
+	var excerciseId = $('#excerciseId').val();
+	return JSON.stringify({
+		"id" : excerciseId == "" ? null : excerciseId,
+		"name" : $('#name').val()
+	});
+}
--- src/main/webapp/js/facebook.js
+++ src/main/webapp/js/facebook.js
@@ -1,23 +1,24 @@
 function statusChangeCallback(response) {
-	console.log('statusChangeCallback');
-	console.log(response);
+	console.info('statusChangeCallback');
+	console.info(response);
 	// The response object is returned with a status field that lets the
 	// app know the current login status of the person.
 	// Full docs on the response object can be found in the documentation
 	// for FB.getLoginStatus().
 	if (response.status === 'connected') {
 		// Logged into your app and Facebook.
-		// testAPI();
-		console.log('connected');
-		console.log(response.authResponse.accessToken);
+
+		console.info('connected');
+		console.info(response.authResponse.accessToken);
+		getUserDetails();
 
 	} else if (response.status === 'not_authorized') {
-		console.log('not_authorized');
+		console.info('not_authorized');
 		// The person is logged into Facebook, but not your app.
 		document.getElementById('status').innerHTML = 'Please log '
 				+ 'into this app.';
 	} else {
-		console.log('other');
+		console.info('other');
 		// The person is not logged into Facebook, so we're not sure if
 		// they are logged into this app or not.
 		document.getElementById('status').innerHTML = 'Please log '
@@ -26,7 +27,7 @@ function statusChangeCallback(response) {
 }
 
 function customLoginHandler() {
-	alert("logged in")
+	alert("customLoginHandler")
 	console.info("doing login")
 	FB.getLoginStatus(function(response) {
 		statusChangeCallback(response);
@@ -36,11 +37,45 @@ function customLoginHandler() {
 
 function getUserDetails() {
 
-	FB.api('/me', function(response) {
-		console.log(JSON.stringify(response));
+	FB.api('/me?fields=email', function(response) {
+		console.info("userDetails")
+		console.info(JSON.stringify(response));
 	});
 
+	var credentials = {};
+	credentials.username = "testUser";
+	credentials.accesscode = "testcode";
+	return credentials;
 }
+
+window.fbAsyncInit = function() {
+	FB.init({
+		appId : '224674001213541',
+		cookie : true, // enable cookies to allow the server to access
+		// the session
+		xfbml : true, // parse social plugins on this page
+		version : 'v2.5' // use graph api version 2.5
+	});
+
+	// Now that we've initialized the JavaScript SDK, we call
+	// FB.getLoginStatus(). This function gets the state of the
+	// person visiting this page and can return one of three states to
+	// the callback you provide. They can be:
+	//
+	// 1. Logged into your app ('connected')
+	// 2. Logged into Facebook, but not your app ('not_authorized')
+	// 3. Not logged into Facebook and can't tell if they are logged into
+	// your app or not.
+	//
+	// These three cases are handled in the callback function.
+
+	// Lets check if we are logged in and then do something depending on if we
+	// are.
+	FB.getLoginStatus(function(response) {
+		statusChangeCallback(response);
+	});
+
+};
 
 (function(d, s, id) {
 	var js, fjs = d.getElementsByTagName(s)[0];
@@ -48,10 +83,8 @@ function getUserDetails() {
 		return;
 	js = d.createElement(s);
 	js.id = id;
-	js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5&appId=224674001213541";
+	js.src = "//connect.facebook.net/en_GB/sdk.js";
 	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-
 console.info("loaded facebook.js")
-
